969984036952ceaf7a836d45d88e99897519cbb3
Prashant Raghav <prashant_raghav@d4.magicbot.in>
2014-09-21 16:41:21 +0000

    BaseUrl is commented at core

--- mvc/core/config.php
+++ mvc/core/config.php
@@ -1,6 +1,6 @@
 <?php
 define('BASE_PATH', dirname(dirname(dirname(realpath(__FILE__)))));
-define('BASE_URL', (isset($_SERVER['HTTPS']))?"https://":'http://'.str_replace($_SERVER['DOCUMENT_ROOT'], $_SERVER['SERVER_NAME'], BASE_PATH));
+//define('BASE_URL', (isset($_SERVER['HTTPS']))?"https://":'http://'.str_replace($_SERVER['DOCUMENT_ROOT'], $_SERVER['SERVER_NAME'], BASE_PATH));
 
 //define('THEME_PATH', (defined('APP_ROOT'))?'/'.APP_ROOT.'/themes/'.THEME:'/themes/'.THEME);
 //define('ADMIN_THEME_PATH', (defined('APP_ROOT'))?'/'.APP_ROOT.'/themes/'.ADMIN_THEME:'/themes/'.ADMIN_THEME);
